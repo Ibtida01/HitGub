0523c446e2b847e9c19e9d88f1e47b7431314354
shakshor <shakshor123@gmail.com>
2026-04-02 03:18:54 +0600

    allowing all origins

--- backend/server.py
+++ backend/server.py
@@ -26,13 +26,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5174",
-    ],
-    allow_origin_regex=r"https?://(localhost|127\.0\.0\.1)(:\\d+)?$",
+    allow_origins=["*"],  # Allows all origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
